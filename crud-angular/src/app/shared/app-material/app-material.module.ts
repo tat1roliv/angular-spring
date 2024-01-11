7c46fe0f27f9bb4c020df647858926ecea886d44
tati oliveira <tat1roliv@gmail.com>
2024-01-11 08:58:27 -0300

    02-07
    table style + create a module for angular material imports

---
+++ crud-angular/src/app/shared/app-material/app-material.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    MatTableModule,
+    MatCardModule,
+    MatToolbarModule
+  ]
+})
+export class AppMaterialModule { }
