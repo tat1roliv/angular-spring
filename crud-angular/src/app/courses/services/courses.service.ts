96ff78e7a58c4a99b5e882c954d581bb3a17fa40
tati oliveira <tat1roliv@gmail.com>
2024-01-16 09:02:19 -0300

    02-09
    http get

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Course } from '../model/course';
 import { HttpClient } from '@angular/common/http';
+import { first, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -18,6 +19,9 @@ export class CoursesService {
   private readonly API = '/assets/courses.json';
 
   list() {
-    return this.httpClient.get<Course[]>(this.API)
+    return this.httpClient.get<Course[]>(this.API).pipe(
+      first(),
+      tap(courses => console.log(courses))
+      );
   }
 }
