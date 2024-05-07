34a7558d7be3399c69adbced0aaa15d273b37bb0
tati oliveira <tat1roliv@gmail.com>
2024-05-07 09:05:42 -0300

    partial

--- crud-spring/crud-spring/src/main/java/com/tat1roliv/crudspring/controller/CourseController.java
+++ crud-spring/crud-spring/src/main/java/com/tat1roliv/crudspring/controller/CourseController.java
@@ -13,6 +13,12 @@ import com.tat1roliv.crudspring.model.Course;
 import com.tat1roliv.crudspring.repository.CourseRepository;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 @RestController
 @RequestMapping("/api/courses")
@@ -22,10 +28,22 @@ public class CourseController {
 
     private final CourseRepository courseRepository;
 
+    
+    public String requestMethodName(@RequestParam String param) {
+        return new String();
+    }
+    
+    //@RequestMapping(method = RequestMethod.GET)
     @GetMapping
     public List<Course> list() {
         return courseRepository.findAll();
  
+    }
+
+    //@RequestMapping(method = RequestMethod.POST)
+    @PostMapping
+    public void create(){
+
     }
 
 }
