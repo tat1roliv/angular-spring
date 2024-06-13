9d20092923861f24bb64ee43d3e2b06ec407e535
tati oliveira <tat1roliv@gmail.com>
2024-06-13 09:19:02 -0300

    partial

--- crud-angular/src/app/courses/courses-list/courses-list.component.ts
+++ crud-angular/src/app/courses/courses-list/courses-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output , EventEmitter} from '@angular/core';
 import { Course } from '../model/course';
 import { ActivatedRoute, Router } from '@angular/router';
 
@@ -10,15 +10,17 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class CoursesListComponent {
 
   @Input() coursesData: Course[] = [];
+  //@Output() add = new EventEmitter(isAsync: false);
+  @Output() add: EventEmitter<boolean> = new EventEmitter(false);
 
-  readonly displayedColumns: string[] = [ "_id", "name", "category", "actions" ]
+  readonly displayedColumns: string[] = [ "_id", "name", "category", "actions" ];
   constructor(
-    public router: Router,
-    private route: ActivatedRoute,) {
+    //public router: Router,
+    // private route: ActivatedRoute,
+   ) { }
 
-  }
-
-  onAdd() {
-    this.router.navigate(['new'], {relativeTo: this.route});
+  onAddComponent() {
+    //this.router.navigate(['new'], {relativeTo: this.route});
+    this.add.emit(true);
   }
 }
