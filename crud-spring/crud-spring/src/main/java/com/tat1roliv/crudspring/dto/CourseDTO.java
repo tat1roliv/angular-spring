83f9a4ae01f539a0fe95700f26b4259ace1734bc
Tati Oliveira <tat1roliv@gmail.com>
2024-10-31 11:42:53 -0300

    07-41
    dto

--- crud-spring/crud-spring/src/main/java/com/tat1roliv/crudspring/dto/CourseDTO.java
+++ crud-spring/crud-spring/src/main/java/com/tat1roliv/crudspring/dto/CourseDTO.java
@@ -6,27 +6,14 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-import lombok.Data;
 
-@Data
-public class CourseDTO {
-    
-    @JsonProperty("_id")
-    private Long id;
 
-    @NotBlank
-    @NotNull
-    @Size(min = 5, max = 100)
-    private String name;
-
-    @NotNull
-    @Size(max = 10)
-    @Pattern(regexp = "Back-end|Front-end")
-    private String category;
-
-    @NotNull
-    @Size(max = 10)
-    @Pattern(regexp = "Active|Inactive")
-    private String status = "Active";
+public record CourseDTO(
+    @JsonProperty("_id") Long id, 
+    @NotBlank @NotNull @Size(min = 5, max = 100) String name, 
+    @NotNull @Size(max = 10) @Pattern(regexp = "Back-end|Front-end") String category
+    //no expor
+    //@NotNull @Size(max = 10) @Pattern(regexp = "Active|Inactive") String status
+) {
 
 }
