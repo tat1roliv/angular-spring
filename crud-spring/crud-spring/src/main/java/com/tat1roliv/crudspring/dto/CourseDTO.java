706cb8d6f04ce9e58d83c5fcdcc2b2dd4e624311
Tati Oliveira <tat1roliv@gmail.com>
2024-12-27 19:34:07 -0300

    09-57
    generic enum validation

--- crud-spring/crud-spring/src/main/java/com/tat1roliv/crudspring/dto/CourseDTO.java
+++ crud-spring/crud-spring/src/main/java/com/tat1roliv/crudspring/dto/CourseDTO.java
@@ -3,19 +3,22 @@ package com.tat1roliv.crudspring.dto;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.tat1roliv.crudspring.enums.Category;
+import com.tat1roliv.crudspring.enums.validation.ValueOfEnum;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
+//import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 
 public record CourseDTO(
     @JsonProperty("_id") Long id, 
     @NotBlank @NotNull @Size(min = 5, max = 100) String name, 
-    @NotNull @Size(max = 10) @Pattern(regexp = "Back-end|Front-end") String category, 
+    //@NotNull @Size(max = 10) @Pattern(regexp = "Back-end|Front-end") String category, 
+    @NotNull @Size(max = 10) @ValueOfEnum(enumClass = Category.class) String category, 
     @NotEmpty @NotNull @Valid List<LessonDTO> lessons
     //no expor
     //@NotNull @Size(max = 10) @Pattern(regexp = "Active|Inactive") String status
---
+++ crud-spring/crud-spring/src/main/java/com/tat1roliv/crudspring/enums/validation/ValueOfEnumValidator.java
@@ -0,0 +1,61 @@
+package com.tat1roliv.crudspring.enums.validation;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class ValueOfEnumValidator implements ConstraintValidator<ValueOfEnum, CharSequence> {
+    private List<String> acceptedValues;
+
+    @Override
+    public void initialize(ValueOfEnum annotation) {
+        acceptedValues = Stream.of(annotation.enumClass().getEnumConstants())
+                .map(Enum::toString)
+                .collect(Collectors.toList());
+    }
+
+    // @Override
+    // public boolean isValid(CharSequence value, ConstraintValidatorContext context) {
+    //     if (value == null) {
+    //         return true;
+    //     }
+
+    //     return acceptedValues.contains(value.toString());
+    // }
+
+    @Override
+
+    public boolean isValid(CharSequence value, ConstraintValidatorContext context) {
+
+        if (value == null || "".equals(value.toString().trim())) {
+
+            return true;
+
+        }
+
+
+
+        if (!acceptedValues.contains(value.toString())) {
+
+            context.disableDefaultConstraintViolation();
+
+            context.buildConstraintViolationWithTemplate(
+
+                    "Valid values " + acceptedValues)
+
+                    .addConstraintViolation();
+
+            return false;
+
+        }
+
+
+
+        return true;
+
+    }
+
+}
